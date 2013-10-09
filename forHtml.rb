d63e17e27b9cb320e1d2b71f603903d6fc2474c4
niuqihong <371464685@qq.com>
2013-10-09 12:06:39 +0800

    update

--- forHtml.rb
+++ forHtml.rb
@@ -1,11 +1,21 @@
-server = %w'http://ruby-china.org/wiki'
+require "open-uri" 
+  #如果有GET请求参数直接写在URI地址中  
+uri = 'http://ruby-china.org/wiki'
+html_response = nil 
+open(uri) do |http|  
+  html_response = http.read  
+end 
 
-r = nil 
+File.open( 'save.txt', 'w' ) {|f|
+ f.write html_response
+}
 
-File.open('test2.out','w') do |s|
-  open(uri) do |http|  
-   r = http.read  
-  end 
-    
-   s.puts r
-end
+# def split_uri( uri_str )
+#   m = %r<http://([^/]+)>.match(uri_str) or raise ArgumentError, "cannot parse URI: #{uri_str}"
+#   host = m[1].strip
+#   path = m.post_match
+#   path = '/' if path.empty?
+#   return host, path
+# end
+
+#print fetch('http://www.ruby-lang.org')
--- getHtml.rb
+++ getHtml.rb
@@ -1,11 +1,17 @@
-require "open-uri" 
-  #如果有GET请求参数直接写在URI地址中  
-uri = 'http://ruby-china.org/wiki'
-html_response = nil 
-open(uri) do |http|  
-  html_response = http.read  
-end 
+require 'net/http'
+Net::HTTP.version_1_2   # 设定对象的运作方式
+
+response = http.get( 'http://www.ruby-lang.org' )
 
 File.open( 'save.txt', 'w' ) {|f|
- f.write html_response
+      f.write response
 }
+# def split_uri( uri_str )
+#   m = %r<http://([^/]+)>.match(uri_str) or raise ArgumentError, "cannot parse URI: #{uri_str}"
+#   host = m[1].strip
+#   path = m.post_match
+#   path = '/' if path.empty?
+#   return host, path
+# end
+
+#print fetch('http://www.ruby-lang.org')
